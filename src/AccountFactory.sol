6be7714293cf1826890b073b79bfb8df040c79d2
Shivanshi Tyagi <shivanshityagi@icloud.com>
2023-11-07 05:17:18 +0700

    add deposit mgmt

--- src/AccountFactory.sol
+++ src/AccountFactory.sol
@@ -1,18 +1,19 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
+import {Ownable} from "@solady/src/auth/Ownable.sol";
 import {LibClone} from "@solady/src/utils/LibClone.sol";
 
 /// @notice Simple extendable smart account factory implementation.
 /// @author nani.eth (https://github.com/nanidao/account/blob/main/src/AccountFactory.sol)
-/// @author Modified from Solady (https://github.com/vectorized/solady/blob/main/src/accounts/ERC4337Factory.sol)
-contract AccountFactory {
+/// @author ERC4337Factory by Solady (https://github.com/vectorized/solady/blob/main/src/accounts/ERC4337Factory.sol)
+contract AccountFactory is Ownable {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                         IMMUTABLES                         */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     /// @dev Address of the Account implementation.
-    address internal immutable implementation;
+    address public immutable implementation;
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                        CONSTRUCTOR                         */
@@ -20,6 +21,17 @@ contract AccountFactory {
 
     constructor(address account) payable {
         implementation = account;
+        _setOwner(tx.origin);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                        ENTRY POINT                         */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Returns the canonical ERC4337 EntryPoint contract.
+    /// Override this function to return a different EntryPoint.
+    function entryPoint() public view virtual returns (address) {
+        return 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -59,5 +71,56 @@ contract AccountFactory {
     /// Used for mining vanity addresses with create2crunch.
     function initCodeHash() public view virtual returns (bytes32) {
         return LibClone.initCodeHashERC1967(implementation);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                     DEPOSIT OPERATIONS                     */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Returns the account's balance on the EntryPoint.
+    function getDeposit() public view virtual returns (uint256 result) {
+        address ep = entryPoint();
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x20, address()) // Store the `account` argument.
+            mstore(0x00, 0x70a08231) // `balanceOf(address)`.
+            result :=
+                mul( // Returns 0 if the EntryPoint does not exist.
+                    mload(0x20),
+                    and( // The arguments of `and` are evaluated from right to left.
+                        gt(returndatasize(), 0x1f), // At least 32 bytes returned.
+                        staticcall(gas(), ep, 0x1c, 0x24, 0x20, 0x20)
+                    )
+                )
+        }
+    }
+
+    /// @dev Deposit more funds for this account in the EntryPoint.
+    function addDeposit() public payable virtual {
+        address ep = entryPoint();
+        /// @solidity memory-safe-assembly
+        assembly {
+            // The EntryPoint has balance accounting logic in the `receive()` function.
+            // forgefmt: disable-next-item
+            if iszero(mul(extcodesize(ep), call(gas(), ep, callvalue(), codesize(), 0x00, codesize(), 0x00))) {
+                revert(codesize(), 0x00) // For gas estimation.
+            }
+        }
+    }
+
+    /// @dev Withdraw ETH from the account's deposit on the EntryPoint.
+    function withdrawDepositTo(address to, uint256 amount) public payable virtual onlyOwner {
+        address ep = entryPoint();
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x14, to) // Store the `to` argument.
+            mstore(0x34, amount) // Store the `amount` argument.
+            mstore(0x00, 0x205c2878000000000000000000000000) // `withdrawTo(address,uint256)`.
+            if iszero(mul(extcodesize(ep), call(gas(), ep, 0, 0x10, 0x44, codesize(), 0x00))) {
+                returndatacopy(mload(0x40), 0x00, returndatasize())
+                revert(mload(0x40), returndatasize())
+            }
+            mstore(0x34, 0) // Restore the part of the free memory pointer that was overwritten.
+        }
     }
 }
