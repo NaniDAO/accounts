97ec3663e59816761ff13eb714416c016a19a3f6
ross <92001561+z0r0z@users.noreply.github.com>
2023-11-08 14:03:49 +0000

    ⚡ Optimize AccountFactory

--- src/AccountFactory.sol
+++ src/AccountFactory.sol
@@ -1,25 +1,29 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
-import {Ownable} from "@solady/src/auth/Ownable.sol";
 import {ERC4337Factory} from "@solady/src/accounts/ERC4337Factory.sol";
 
 /// @notice Simple extendable smart account factory implementation.
 /// @author nani.eth (https://github.com/nanidao/account/blob/main/src/AccountFactory.sol)
-contract AccountFactory is Ownable, ERC4337Factory {
+contract AccountFactory is ERC4337Factory {
+    /// @dev Holds an immutable owner.
+    address internal immutable _OWNER;
+
     /// @dev Constructs this factory to deploy the implementation.
+    /// Additionally, sets owner account for peripheral concerns.
     constructor(address erc4337) payable ERC4337Factory(erc4337) {
-        _setOwner(tx.origin);
+        _OWNER = ERC4337Factory.createAccount(tx.origin, 0);
     }
 
-    /// @dev Execute a call from this factory for staking operations.
+    /// @dev Execute a call from this factory for peripheral concerns.
     function execute(address target, uint256 value, bytes calldata data)
         public
         payable
         virtual
-        onlyOwner
         returns (bytes memory result)
     {
+        // Only the owner can call.
+        assert(msg.sender == _OWNER);
         /// @solidity memory-safe-assembly
         assembly {
             result := mload(0x40)
