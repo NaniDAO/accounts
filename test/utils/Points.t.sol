a8b95810a7f98b83e98852f89a9d705ebd28ca2e
ross <92001561+z0r0z@users.noreply.github.com>
2023-12-15 13:13:22 +0000

    nits

--- test/utils/Points.t.sol
+++ test/utils/Points.t.sol
@@ -13,12 +13,13 @@ contract PointsTest is Test {
     Points points;
     address token;
 
+    MockERC4337 account;
+
     uint256 constant POT = 1_000_000_000;
 
     function setUp() public {
         (alice, alicePk) = makeAddrAndKey("alice");
         bob = makeAddr("bob");
-        MockERC4337 account;
         points = new Points(address(account = new MockERC4337()), 1);
         account.initialize(alice);
         token = address(new MockERC20("TEST", "TEST", 18));
@@ -35,7 +36,7 @@ contract PointsTest is Test {
         vm.assume(bonus < POT);
         uint256 start = 1;
         vm.warp(42);
-        uint256 bal = points.check(bob, start, bonus,  sign(alicePk, bob, start, bonus));
+        uint256 bal = points.check(bob, start, bonus, sign(alicePk, bob, start, bonus));
         assertEq(bal, bonus + 41);
     }
 
@@ -44,7 +45,7 @@ contract PointsTest is Test {
         uint256 start = 1;
         vm.warp(42);
         vm.prank(bob);
-        points.claim(IERC20(token), start, bonus,  sign(alicePk, bob, start, bonus));
+        points.claim(IERC20(token), start, bonus, sign(alicePk, bob, start, bonus));
         assertEq(MockERC20(token).balanceOf(bob), bonus + 41);
     }
 
@@ -58,10 +59,37 @@ contract PointsTest is Test {
         points.claim(IERC20(token), start, bonus, sign(alicePk, bob, start, bonus));
     }
 
-    function sign(uint256 pK, address user, uint256 start, uint256 bonus) internal pure returns (bytes memory signature) {
+    function sign(uint256 pK, address user, uint256 start, uint256 bonus)
+        internal
+        view
+        returns (bytes memory signature)
+    {
+        bytes32 hash = keccak256(abi.encodePacked(user, start, bonus));
+        bytes32 rehash = keccak256(abi.encodePacked("\x19\x01", _buildDomainSeparator(), hash));
         (uint8 v, bytes32 r, bytes32 s) =
-            vm.sign(pK, keccak256(abi.encodePacked(user, start, bonus)));
-
+            vm.sign(pK, keccak256(abi.encode(address(account), rehash)));
         return abi.encodePacked(r, s, v);
     }
+
+    /// @dev Returns the EIP-712 domain separator.
+    function _buildDomainSeparator() private view returns (bytes32 separator) {
+        // We will use `separator` to store the name hash to save a bit of gas.
+        separator = keccak256(bytes("NANI"));
+        bytes32 versionHash = keccak256(bytes("0.0.0"));
+        address _account = address(account);
+        /// @solidity memory-safe-assembly
+        assembly {
+            let m := mload(0x40) // Load the free memory pointer.
+            mstore(m, _DOMAIN_TYPEHASH)
+            mstore(add(m, 0x20), separator) // Name hash.
+            mstore(add(m, 0x40), versionHash)
+            mstore(add(m, 0x60), chainid())
+            mstore(add(m, 0x80), _account)
+            separator := keccak256(m, 0xa0)
+        }
+    }
+
+    /// @dev `keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")`.
+    bytes32 internal constant _DOMAIN_TYPEHASH =
+        0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
 }
