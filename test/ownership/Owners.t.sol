5f775170a9a0c748560f37c74c2b8d674b6f5d8c
ross <92001561+z0r0z@users.noreply.github.com>
2023-12-28 10:31:33 +0000

    🪡 Extend tkn tests

--- test/ownership/Owners.t.sol
+++ test/ownership/Owners.t.sol
@@ -3,9 +3,59 @@ pragma solidity ^0.8.19;
 
 import "@forge/Test.sol";
 import "@solady/test/utils/mocks/MockERC20.sol";
+import "@solady/test/utils/mocks/MockERC721.sol";
+import "@solady/test/utils/mocks/MockERC1155.sol";
+import "@solady/test/utils/mocks/MockERC6909.sol";
 
 import {Account as NaniAccount} from "../../src/Account.sol";
 import {ITokenOwner, ITokenAuth, Owners} from "../../src/ownership/Owners.sol";
+
+contract MockERC721TotalSupply is MockERC721 {
+    uint256 public totalSupply;
+
+    constructor() payable {}
+
+    function mint(address to, uint256 id) public virtual override(MockERC721) {
+        _mint(to, id);
+
+        unchecked {
+            ++totalSupply;
+        }
+    }
+}
+
+contract MockERC1155TotalSupply is MockERC1155 {
+    mapping(uint256 => uint256) public totalSupply;
+
+    constructor() payable {}
+
+    function mint(address to, uint256 id, uint256 amount, bytes memory)
+        public
+        virtual
+        override(MockERC1155)
+    {
+        _mint(to, id, amount, "");
+
+        totalSupply[id] += amount;
+    }
+}
+
+contract MockERC6909TotalSupply is MockERC6909 {
+    mapping(uint256 => uint256) public totalSupply;
+
+    constructor() payable {}
+
+    function mint(address to, uint256 id, uint256 amount)
+        public
+        payable
+        virtual
+        override(MockERC6909)
+    {
+        _mint(to, id, amount);
+
+        totalSupply[id] += amount;
+    }
+}
 
 contract OwnersTest is Test {
     address internal alice;
@@ -18,6 +68,9 @@ contract OwnersTest is Test {
     uint256 internal davePk;
 
     address internal erc20;
+    address internal erc721;
+    address internal erc1155;
+    address internal erc6909;
 
     NaniAccount internal account;
     Owners internal owners;
@@ -25,7 +78,7 @@ contract OwnersTest is Test {
 
     mapping(address => uint256) keys;
 
-    function setUp() public {
+    function setUp() public payable {
         (alice, alicePk) = makeAddrAndKey("alice");
         keys[alice] = alicePk;
         (bob, bobPk) = makeAddrAndKey("bob");
@@ -35,15 +88,35 @@ contract OwnersTest is Test {
         (dave, davePk) = makeAddrAndKey("dave");
         keys[dave] = davePk;
 
+        account = new NaniAccount();
+        account.initialize(alice);
+        owners = new Owners();
+
+        uint256 accountId = uint256(keccak256(abi.encodePacked(address(account))));
+
         erc20 = address(new MockERC20("TEST", "TEST", 18));
         MockERC20(erc20).mint(alice, 40 ether);
         MockERC20(erc20).mint(bob, 20 ether);
         MockERC20(erc20).mint(chuck, 20 ether);
         MockERC20(erc20).mint(dave, 20 ether);
 
-        account = new NaniAccount();
-        account.initialize(alice);
-        owners = new Owners();
+        erc721 = address(new MockERC721TotalSupply());
+        MockERC721TotalSupply(erc721).mint(alice, 0);
+        MockERC721TotalSupply(erc721).mint(bob, 1);
+        MockERC721TotalSupply(erc721).mint(chuck, 2);
+        MockERC721TotalSupply(erc721).mint(dave, 3);
+
+        erc1155 = address(new MockERC1155TotalSupply());
+        MockERC1155TotalSupply(erc1155).mint(alice, accountId, 40 ether, "");
+        MockERC1155TotalSupply(erc1155).mint(bob, accountId, 20 ether, "");
+        MockERC1155TotalSupply(erc1155).mint(chuck, accountId, 20 ether, "");
+        MockERC1155TotalSupply(erc1155).mint(dave, accountId, 20 ether, "");
+
+        erc6909 = address(new MockERC6909TotalSupply());
+        MockERC6909TotalSupply(erc6909).mint(alice, accountId, 40 ether);
+        MockERC6909TotalSupply(erc6909).mint(bob, accountId, 20 ether);
+        MockERC6909TotalSupply(erc6909).mint(chuck, accountId, 20 ether);
+        MockERC6909TotalSupply(erc6909).mint(dave, accountId, 20 ether);
     }
 
     function testDeploy() public {
@@ -463,6 +536,394 @@ contract OwnersTest is Test {
         bytes32 signHash = _toEthSignedMessageHash(userOpHash);
         _owners = _sortAddresses(memOwners);
         userOp.signature = abi.encodePacked(_owners[0], _sign(_getPkByAddr(_owners[0]), signHash));
+
+        vm.prank(entryPoint);
+        uint256 validationData = account.validateUserOp(userOp, userOpHash, 0);
+        assertEq(validationData, 0x00);
+    }
+
+    // In 2-of-3, at least 2 ERC721 units signed.
+    function testIsValidSignatureWeightedERC721() public payable {
+        address[] memory _owners = new address[](0);
+        uint256[] memory _shares = new uint256[](0);
+
+        address[] memory memOwners = new address[](3);
+        memOwners[0] = alice;
+        memOwners[1] = bob;
+        memOwners[2] = chuck;
+
+        vm.prank(alice);
+        account.execute(
+            address(owners),
+            0,
+            abi.encodeWithSelector(
+                owners.install.selector,
+                _owners,
+                _shares,
+                ITokenOwner(erc721),
+                Owners.TokenStandard.ERC721,
+                2,
+                "",
+                ITokenAuth(address(0))
+            )
+        );
+
+        vm.prank(alice);
+        account.execute(
+            address(account),
+            0,
+            abi.encodeWithSelector(account.completeOwnershipHandover.selector, address(owners))
+        );
+
+        NaniAccount.UserOperation memory userOp;
+        bytes32 userOpHash = keccak256("OWN");
+        bytes32 signHash = _toEthSignedMessageHash(userOpHash);
+        _owners = _sortAddresses(memOwners);
+        userOp.signature = abi.encodePacked(
+            _owners[0],
+            _sign(_getPkByAddr(_owners[0]), signHash),
+            _owners[1],
+            _sign(_getPkByAddr(_owners[1]), signHash)
+        );
+
+        vm.prank(entryPoint);
+        uint256 validationData = account.validateUserOp(userOp, userOpHash, 0);
+        assertEq(validationData, 0x00);
+    }
+
+    // In 2-of-3, only 1 ERC721 units signed. So fail.
+    function testFailIsValidSignatureWeightedERC721() public payable {
+        address[] memory _owners = new address[](0);
+        uint256[] memory _shares = new uint256[](0);
+
+        address[] memory memOwners = new address[](3);
+        memOwners[0] = alice;
+        memOwners[1] = bob;
+        memOwners[2] = chuck;
+
+        vm.prank(alice);
+        account.execute(
+            address(owners),
+            0,
+            abi.encodeWithSelector(
+                owners.install.selector,
+                _owners,
+                _shares,
+                ITokenOwner(erc721),
+                Owners.TokenStandard.ERC721,
+                2,
+                "",
+                ITokenAuth(address(0))
+            )
+        );
+
+        vm.prank(alice);
+        account.execute(
+            address(account),
+            0,
+            abi.encodeWithSelector(account.completeOwnershipHandover.selector, address(owners))
+        );
+
+        NaniAccount.UserOperation memory userOp;
+        bytes32 userOpHash = keccak256("OWN");
+        bytes32 signHash = _toEthSignedMessageHash(userOpHash);
+        _owners = _sortAddresses(memOwners);
+        userOp.signature = abi.encodePacked(_owners[0], _sign(_getPkByAddr(_owners[0]), signHash));
+
+        vm.prank(entryPoint);
+        uint256 validationData = account.validateUserOp(userOp, userOpHash, 0);
+        assertEq(validationData, 0x00);
+    }
+
+    // In 40-of-100, at least 40 ERC1155 units signed.
+    function testIsValidSignatureWeightedERC1155() public payable {
+        address[] memory _owners = new address[](0);
+        uint256[] memory _shares = new uint256[](0);
+
+        address[] memory memOwners = new address[](3);
+        memOwners[0] = alice;
+        memOwners[1] = bob;
+        memOwners[2] = chuck;
+
+        vm.prank(alice);
+        account.execute(
+            address(owners),
+            0,
+            abi.encodeWithSelector(
+                owners.install.selector,
+                _owners,
+                _shares,
+                ITokenOwner(erc1155),
+                Owners.TokenStandard.ERC1155,
+                40 ether,
+                "",
+                ITokenAuth(address(0))
+            )
+        );
+
+        vm.prank(alice);
+        account.execute(
+            address(account),
+            0,
+            abi.encodeWithSelector(account.completeOwnershipHandover.selector, address(owners))
+        );
+
+        NaniAccount.UserOperation memory userOp;
+        bytes32 userOpHash = keccak256("OWN");
+        bytes32 signHash = _toEthSignedMessageHash(userOpHash);
+        _owners = _sortAddresses(memOwners);
+        userOp.signature = abi.encodePacked(
+            _owners[0],
+            _sign(_getPkByAddr(_owners[0]), signHash),
+            _owners[1],
+            _sign(_getPkByAddr(_owners[1]), signHash),
+            _owners[2],
+            _sign(_getPkByAddr(_owners[2]), signHash)
+        );
+
+        vm.prank(entryPoint);
+        uint256 validationData = account.validateUserOp(userOp, userOpHash, 0);
+        assertEq(validationData, 0x00);
+    }
+
+    // In 40-of-100, 20 ERC1155 units signed. So fail.
+    function testFailIsValidSignatureWeightedERC1155() public payable {
+        address[] memory _owners = new address[](0);
+        uint256[] memory _shares = new uint256[](0);
+
+        address[] memory memOwners = new address[](3);
+        memOwners[0] = alice;
+        memOwners[1] = bob;
+        memOwners[2] = chuck;
+
+        vm.prank(alice);
+        account.execute(
+            address(owners),
+            0,
+            abi.encodeWithSelector(
+                owners.install.selector,
+                _owners,
+                _shares,
+                ITokenOwner(erc1155),
+                Owners.TokenStandard.ERC1155,
+                40 ether,
+                "",
+                ITokenAuth(address(0))
+            )
+        );
+
+        vm.prank(alice);
+        account.execute(
+            address(account),
+            0,
+            abi.encodeWithSelector(account.completeOwnershipHandover.selector, address(owners))
+        );
+
+        NaniAccount.UserOperation memory userOp;
+        bytes32 userOpHash = keccak256("OWN");
+        bytes32 signHash = _toEthSignedMessageHash(userOpHash);
+        _owners = _sortAddresses(memOwners);
+        userOp.signature = abi.encodePacked(_owners[0], _sign(_getPkByAddr(_owners[0]), signHash));
+
+        vm.prank(entryPoint);
+        uint256 validationData = account.validateUserOp(userOp, userOpHash, 0);
+        assertEq(validationData, 0x00);
+    }
+
+    // In 40-of-100, at least 40 ERC6909 units signed.
+    function testIsValidSignatureWeightedERC6909() public payable {
+        address[] memory _owners = new address[](0);
+        uint256[] memory _shares = new uint256[](0);
+
+        address[] memory memOwners = new address[](3);
+        memOwners[0] = alice;
+        memOwners[1] = bob;
+        memOwners[2] = chuck;
+
+        vm.prank(alice);
+        account.execute(
+            address(owners),
+            0,
+            abi.encodeWithSelector(
+                owners.install.selector,
+                _owners,
+                _shares,
+                ITokenOwner(erc6909),
+                Owners.TokenStandard.ERC6909,
+                40 ether,
+                "",
+                ITokenAuth(address(0))
+            )
+        );
+
+        vm.prank(alice);
+        account.execute(
+            address(account),
+            0,
+            abi.encodeWithSelector(account.completeOwnershipHandover.selector, address(owners))
+        );
+
+        NaniAccount.UserOperation memory userOp;
+        bytes32 userOpHash = keccak256("OWN");
+        bytes32 signHash = _toEthSignedMessageHash(userOpHash);
+        _owners = _sortAddresses(memOwners);
+        userOp.signature = abi.encodePacked(
+            _owners[0],
+            _sign(_getPkByAddr(_owners[0]), signHash),
+            _owners[1],
+            _sign(_getPkByAddr(_owners[1]), signHash),
+            _owners[2],
+            _sign(_getPkByAddr(_owners[2]), signHash)
+        );
+
+        vm.prank(entryPoint);
+        uint256 validationData = account.validateUserOp(userOp, userOpHash, 0);
+        assertEq(validationData, 0x00);
+    }
+
+    // In 40-of-100, 20 ERC6909 units signed. So fail.
+    function testFailIsValidSignatureWeightedERC6909() public payable {
+        address[] memory _owners = new address[](0);
+        uint256[] memory _shares = new uint256[](0);
+
+        address[] memory memOwners = new address[](3);
+        memOwners[0] = alice;
+        memOwners[1] = bob;
+        memOwners[2] = chuck;
+
+        vm.prank(alice);
+        account.execute(
+            address(owners),
+            0,
+            abi.encodeWithSelector(
+                owners.install.selector,
+                _owners,
+                _shares,
+                ITokenOwner(erc6909),
+                Owners.TokenStandard.ERC6909,
+                40 ether,
+                "",
+                ITokenAuth(address(0))
+            )
+        );
+
+        vm.prank(alice);
+        account.execute(
+            address(account),
+            0,
+            abi.encodeWithSelector(account.completeOwnershipHandover.selector, address(owners))
+        );
+
+        NaniAccount.UserOperation memory userOp;
+        bytes32 userOpHash = keccak256("OWN");
+        bytes32 signHash = _toEthSignedMessageHash(userOpHash);
+        _owners = _sortAddresses(memOwners);
+        userOp.signature = abi.encodePacked(_owners[0], _sign(_getPkByAddr(_owners[0]), signHash));
+
+        vm.prank(entryPoint);
+        uint256 validationData = account.validateUserOp(userOp, userOpHash, 0);
+        assertEq(validationData, 0x00);
+    }
+
+    function testFailIsValidSignatureOutOfOrder() public payable {
+        address[] memory _owners = new address[](4);
+        uint256[] memory _shares = new uint256[](4);
+        _owners[0] = alice;
+        _shares[0] = 40;
+        _owners[1] = bob;
+        _shares[1] = 20;
+        _owners[2] = chuck;
+        _shares[2] = 20;
+        _owners[3] = dave;
+        _shares[3] = 20;
+
+        vm.prank(alice);
+        account.execute(
+            address(owners),
+            0,
+            abi.encodeWithSelector(
+                owners.install.selector,
+                _owners,
+                _shares,
+                ITokenOwner(address(0)),
+                Owners.TokenStandard.OWN,
+                40,
+                "",
+                ITokenAuth(address(0))
+            )
+        );
+
+        vm.prank(alice);
+        account.execute(
+            address(account),
+            0,
+            abi.encodeWithSelector(account.completeOwnershipHandover.selector, address(owners))
+        );
+
+        NaniAccount.UserOperation memory userOp;
+        bytes32 userOpHash = keccak256("OWN");
+        bytes32 signHash = _toEthSignedMessageHash(userOpHash);
+        userOp.signature = abi.encodePacked(
+            _owners[0],
+            _sign(_getPkByAddr(_owners[0]), signHash),
+            _owners[1],
+            _sign(_getPkByAddr(_owners[1]), signHash),
+            _owners[2],
+            _sign(_getPkByAddr(_owners[2]), signHash)
+        );
+
+        vm.prank(entryPoint);
+        uint256 validationData = account.validateUserOp(userOp, userOpHash, 0);
+        assertEq(validationData, 0x00);
+    }
+
+    function testFailIsValidSignatureInvalidTokenCode() public payable {
+        address[] memory _owners = new address[](4);
+        uint256[] memory _shares = new uint256[](4);
+        _owners[0] = alice;
+        _shares[0] = 40;
+        _owners[1] = bob;
+        _shares[1] = 20;
+        _owners[2] = chuck;
+        _shares[2] = 20;
+        _owners[3] = dave;
+        _shares[3] = 20;
+
+        vm.prank(alice);
+        account.execute(
+            address(owners),
+            0,
+            abi.encodeWithSelector(
+                owners.install.selector,
+                _owners,
+                _shares,
+                ITokenOwner(address(0)),
+                9, // Bad Code.
+                40,
+                "",
+                ITokenAuth(address(0))
+            )
+        );
+
+        vm.prank(alice);
+        account.execute(
+            address(account),
+            0,
+            abi.encodeWithSelector(account.completeOwnershipHandover.selector, address(owners))
+        );
+
+        NaniAccount.UserOperation memory userOp;
+        bytes32 userOpHash = keccak256("OWN");
+        bytes32 signHash = _toEthSignedMessageHash(userOpHash);
+        _owners = _sortAddresses(_owners);
+        userOp.signature = abi.encodePacked(
+            _owners[0],
+            _sign(_getPkByAddr(_owners[0]), signHash),
+            _owners[1],
+            _sign(_getPkByAddr(_owners[1]), signHash),
+            _owners[2],
+            _sign(_getPkByAddr(_owners[2]), signHash)
+        );
 
         vm.prank(entryPoint);
         uint256 validationData = account.validateUserOp(userOp, userOpHash, 0);
