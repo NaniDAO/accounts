c3384859c04ab5906686be3f4e45415b7f75c5ae
shiv <shivanshityagi@icloud.com>
2024-01-21 16:00:23 +0000

    🪡 Invites SVG

--- src/governance/Invites.sol
+++ src/governance/Invites.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
+import {Base64} from "@solady/src/utils/Base64.sol";
 import {ERC721} from "@solady/src/tokens/ERC721.sol";
+import {LibString} from "@solady/src/utils/LibString.sol";
 
 /// @notice Simple NFT contract for sending out custom invites.
 contract Invites is ERC721 {
@@ -20,8 +22,8 @@ contract Invites is ERC721 {
 
     /// ========================== STORAGE ========================== ///
 
-    /// @dev Internal token ID metadata mapping.
-    mapping(uint256 id => string metadata) internal _uris;
+    /// @dev Message for each invite.
+    mapping(uint256 id => string message) public messages;
 
     /// @dev Timestamp for each user's last invitation sent.
     mapping(address user => uint256 timestamp) public lastSent;
@@ -36,14 +38,10 @@ contract Invites is ERC721 {
 
     /// ============================ MINT ============================ ///
 
-    /// @dev Sends out invite NFT for `to` with `message` and `uri`.
-    /// If not the owner (0) account, `msg.sender` must have NFT,
+    /// @dev Sends out invite NFT for `to` with `message`.
+    /// If not owner (0) account, `msg.sender` must have NFT,
     /// as well as pass the timed delay after their first mint.
-    function invite(address to, string calldata message, string calldata uri)
-        public
-        payable
-        virtual
-    {
+    function invite(address to, string calldata message) public payable virtual {
         if (msg.sender != ownerOf(0)) {
             if (balanceOf(msg.sender) == 0) {
                 revert Unauthorized();
@@ -55,9 +53,9 @@ contract Invites is ERC721 {
             }
             lastSent[msg.sender] = block.timestamp;
         }
-        uint256 tokenId = uint256(keccak256(bytes(message)));
-        _uris[tokenId] = uri;
-        _mint(to, tokenId);
+        uint256 id = uint256(keccak256(bytes(message)));
+        messages[id] = message;
+        _mint(to, id);
     }
 
     /// ====================== ERC721 METADATA ====================== ///
@@ -74,6 +72,122 @@ contract Invites is ERC721 {
 
     /// @dev Returns the Uniform Resource Identifier (URI) for token `id`.
     function tokenURI(uint256 id) public view virtual override returns (string memory) {
-        return _uris[id];
+        return _createURI(id);
+    }
+
+    /// @dev Creates the URI for token `id`.
+    function _createURI(uint256 id) internal view virtual returns (string memory) {
+        return string(
+            abi.encodePacked(
+                "data:application/json;base64,",
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name":"',
+                            LibString.concat(unicode"💌", "Invite"),
+                            '","description":"You are cordially invited to alpha test NANI."}',
+                            ',"image":"',
+                            _createImage(id),
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+    function _createImage(uint256 id) internal view virtual returns (string memory) {
+        return string(
+            abi.encodePacked(
+                "data:image/svg+xml;base64,",
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><rect width="101%" height="101%"/><text x="51%" y="21%" dominant-baseline="middle" text-anchor="middle" font-size="6" font-family="\'Courier New\', monospace" fill="#fff">',
+                            unicode"ｊｏｉｎ ｕｓ ((¯͒ . ¯͒))",
+                            '</text><text x="50%" y="35%" dominant-baseline="middle" text-anchor="middle" font-size="4" font-family="\'Courier New\', monospace" fill="#fff">',
+                            unicode"ｆｏｒ ａｌｐｈａ ｔｅｓｔｉｎｇ",
+                            '</text><text x="50%" y="60%" dominant-baseline="middle" text-anchor="middle" font-size="15" font-family="\'Courier New\', monospace" fill="#fff">',
+                            unicode"【ｎａｎｉ】",
+                            '</text><text x="50%" y="80%" dominant-baseline="middle" text-anchor="middle" font-size="4" font-style="italic" font-family="\'Courier New\', monospace" fill="#fff">"',
+                            messages[id],
+                            '"</text><text x="50%" y="5%" dominant-baseline="start" text-anchor="middle" font-size="3" fill="yellow">',
+                            unicode"【⌘】░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░【⌘】",
+                            '</text><text x="50%" y="97%" dominant-baseline="start" text-anchor="middle" font-size="3" fill="yellow">',
+                            unicode"【⌘】░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░-░【⌘】",
+                            '</text><text x="5%" y="10%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="15%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="20%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="25%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="30%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="35%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="40%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="45%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="55%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="60%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="65%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="70%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="75%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="80%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="85%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="5%" y="90%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="10%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="15%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="20%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="25%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="30%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="35%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="40%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="45%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="55%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="60%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="65%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="70%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="75%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="80%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="85%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            '</text><text x="95%" y="90%" dominant-baseline="middle" text-anchor="middle" font-size="3" fill="#fff">',
+                            unicode"░",
+                            "</text></svg>"
+                        )
+                    )
+                )
+            )
+        );
     }
 }
