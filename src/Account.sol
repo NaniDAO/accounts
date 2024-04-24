a3ec83db8b2fd9c8d336ab39b1edfbd8c06c092e
ross <92001561+z0r0z@users.noreply.github.com>
2024-04-24 13:32:16 +0000

    🗝️ Passkey auth flow

--- src/Account.sol
+++ src/Account.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import {ERC4337} from "@solady/src/accounts/ERC4337.sol";
 
-/// @notice Simple extendable smart account implementation.
+/// @notice Simple extendable smart account implementation. Includes passkey auth.
 /// @author nani.eth (https://github.com/NaniDAO/accounts/blob/main/src/Account.sol)
 contract Account is ERC4337 {
     /// @dev Constructs
@@ -16,7 +16,7 @@ contract Account is ERC4337 {
         internal
         pure
         virtual
-        override
+        override(ERC4337)
         returns (string memory, string memory)
     {
         return ("NANI", "1.0.0");
@@ -32,7 +32,7 @@ contract Account is ERC4337 {
         external
         payable
         virtual
-        override
+        override(ERC4337)
         onlyEntryPoint
         payPrefund(missingAccountFunds)
         returns (uint256 validationData)
@@ -42,7 +42,7 @@ contract Account is ERC4337 {
             : _validateUserOp();
     }
 
-    /// @dev Extends validation by forwarding calldata to validator.
+    /// @dev Extends ERC4337 userOp validation by forwarding calldata to validator.
     function _validateUserOp() internal virtual returns (uint256 validationData) {
         assembly ("memory-safe") {
             calldatacopy(0x00, 0x00, calldatasize())
@@ -64,6 +64,32 @@ contract Account is ERC4337 {
             }
             // Return `validationData` if call succeeds.
             validationData := mload(0x00)
+        }
+    }
+
+    /// @dev Extends ERC1271 signature verification with ERC7212 passkeys.
+    function isValidSignature(bytes32 hash, bytes calldata signature)
+        public
+        view
+        virtual
+        override(ERC4337)
+        returns (bytes4 result)
+    {
+        if (signature.length == 128) {
+            // ERC7212 passkey auth flow.
+            (, bytes memory ret) = block.chainid != 1
+                ? address(0x100).staticcall(abi.encodePacked(hash, signature))
+                : address(0x0b).staticcall(abi.encodePacked(hash, signature));
+            bool success = abi.decode(ret, (uint256)) == 1
+                && storageLoad( /*x*/ bytes32(signature[64:96])) /*y*/ == bytes32(signature[96:128]);
+            assembly ("memory-safe") {
+                // `success ? bytes4(keccak256("isValidSignature(bytes32,bytes)")) : 0xffffffff`.
+                // We use `0xffffffff` for invalid, in convention with the reference implementation.
+                result := shl(224, or(0x1626ba7e, sub(0, iszero(success))))
+            }
+        } else {
+            // Solady ERC1271 auth flow.
+            result = super.isValidSignature(hash, signature);
         }
     }
 }
